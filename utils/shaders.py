4eb89581c42bd76da3536ba20d9589bba93eab37
thevarunsharma <varunsharma123.del@gmail.com>
2019-03-21 02:09:44 +0530

    added camera feed display feature in anim window

---
+++ utils/shaders.py
@@ -0,0 +1,19 @@
+img_vertex = """
+    attribute vec2 position;
+    attribute vec2 texcoord;
+    varying vec2 v_texcoord;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+        v_texcoord = texcoord;
+    }
+"""
+
+img_fragment = """
+    uniform sampler2D texture;
+    varying vec2 v_texcoord;
+    void main()
+    {
+        gl_FragColor = texture2D(texture, v_texcoord);
+    }
+"""
